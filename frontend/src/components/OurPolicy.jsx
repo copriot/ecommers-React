b7f9ba1e8ff90fd75313771d9c2ccb122d43fadf
Zeki <dev.zek@192.168.1.56>
2024-08-23 22:27:49 +0300

    Then let the one who is without sin throw the stone.OurPolicy and BestSeller component prepared printed home page

---
+++ frontend/src/components/OurPolicy.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { assets } from "../assets/frontend_assets/assets";
+
+const OurPolicy = () => {
+  return (
+    <div className="flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700">
+      <div>
+        <img src={assets.exchange_icon} className="w-12 m-auto mb-5" alt="" />
+        <p className="font-semibold">Easy Exchange Policy</p>
+        <p className="text-gray-400">We offer hassle free exchange policy</p>
+      </div>
+    </div>
+  );
+};
+
+export default OurPolicy;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
 import LatestCollection from "../components/LatestCollection";
+import BestSeller from "../components/BestSeller";
+import OurPolicy from "../components/OurPolicy";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <LatestCollection />
+      <BestSeller />
+      <OurPolicy />
     </div>
   );
 };
