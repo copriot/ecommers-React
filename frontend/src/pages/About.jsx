11631749c3267478bb8ece0507052f172fda5a8f
Zeki <dev.zek@Zeki-MacBook-Pro.local>
2024-08-26 19:26:46 +0300

    latest version Finished

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,67 @@
 import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <div className="text-2xl text-center pt-8 border-t">
+        <Title text1={"ABOUT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img
+          className="w-full md:max-w-[450px]"
+          src={assets.about_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae
+            repudiandae dolores quidem voluptate sunt laudantium delectus quis
+            eum nam doloribus.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+            earum officia labore ducimus mollitia reprehenderit quae optio
+            accusamus voluptatum voluptates?
+          </p>
+          <b className="text-gray-800">Our Mission</b>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor,
+            debitis!
+          </p>
+        </div>
+      </div>
+      <div className="text-4xl py-4">
+        <Title text1={"WHY"} text2={"CHOOSE US"} />
+      </div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance :</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt,
+            ratione?
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convernience :</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt,
+            ratione?
+          </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service :</b>
+          <p className="text-gray-600">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt,
+            ratione?
+          </p>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default About;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import Title from "../components/Title";
+import { assets } from "../assets/frontend_assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <div>
+        <div className="text-center text-2xl pt-10 border-t">
+          <Title text1={"CONTACT"} text2={"US"} />
+        </div>
+
+        <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+          <img
+            src={assets.contact_img}
+            className="w-full md:max-w-[480px]"
+            alt=""
+          />
+          <div className="flex flex-col justify-center  gap-10 mb-28">
+            <p className="font-semibold text-xl text-gray-600">Our Store</p>
+            <p className="text-gray-500">
+              54709 Willms Station <br /> Suite 350, Washington, USA
+            </p>
+            <p className="text-gray-500">
+              Tel: (999) 999-9999 <br />
+              Email: admin@forever.com
+            </p>
+            <p className="font-semibold text-xl text-gray-600">
+              Careers at Forever
+            </p>
+            <p className="text-gray-500">
+              Learn more about our teams and job openings.
+            </p>
+            <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+              Explore Jobs
+            </button>
+          </div>
+        </div>
+      </div>
+      <NewsletterBox />
+    </div>
+  );
 };
 
 export default Contact;
